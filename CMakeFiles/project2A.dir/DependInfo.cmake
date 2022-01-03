ac079b9e758968fb842aa852ea2af2f0c2bd2fc9
Jarett <54662456+jarettn18@users.noreply.github.com>
2022-01-03 11:05:50 -0800

    Code Base Init

---
+++ CMakeFiles/project2A.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/jaret/source/repos/441/441/2A/project2A.cxx" "/mnt/c/Users/jaret/source/repos/441/441/2A/CMakeFiles/project2A.dir/project2A.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
